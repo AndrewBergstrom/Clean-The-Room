344ccb29512a912357c11a82322f592a35d8fbbb
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-02-18 14:24:28 -1000

    fixed bug

--- script.js
+++ script.js
@@ -11,4 +11,6 @@ console.log([...new Set(myArr)].sort((a,b) => a - b).reduce((accumulator, next) 
 
 // Question 2: Write a javascript function that takes an array of numbers and a target number. The function should find two different numbers in the array that, when added together, give the target number. For example: answer([1,2,3], 4)should return [1,3]
 
+
+
 // Question 3: Write a function that converts HEX to RGB. Then Make that function auto-dect the formats so that if you enter HEX color format it returns RGB and if you enter RGB color format it returns HEX.
